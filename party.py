0cf0c5771641b213e7b4cf58c34cc6878df1fe93
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-14 14:08:37 -0700

    Completed 'most_and_least_common_type' doctests

--- party.py
+++ party.py
@@ -26,6 +26,31 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+    
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ...     ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+        
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ...     {'type': 'water'},
+        ...     ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
+    
     """
 
     types = {}
